2041ae3d200c3b66c0b31c04538eb6157dd26e2b
Sergei Maertens <sergei@maykinmedia.nl>
2023-01-18 23:12:56 +0100

    :sparkles: POC TokenRow component

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
 
-export interface TokenRowProps {
-    prefix: string;
+export interface DesignToken {
+  name: string;
+  value: string;
+  original: {
+    value: string;
+  }
+  path: string[];
+  attributes: {
+    [key: string]: string;
+  }
 }
 
-const TokenRow = ({prefix}: TokenRowProps): JSX.Element => {
-  return <div>{prefix}: ello</div>;
+export interface TokenRowProps {
+  designToken: DesignToken;
+}
+
+const TokenRow = ({designToken}: TokenRowProps): JSX.Element => {
+  const { value, original, path } = designToken;
+  const tokenPath = path.join('.');
+  return (
+    <tr>
+      <td><code>{tokenPath}</code></td>
+      <td>
+        <input name={tokenPath} type="text" placeholder={value} defaultValue="" />
+      </td>
+      <td>
+        <code>{original.value}</code>
+      </td>
+    </tr>
+  );
 };
 
 
