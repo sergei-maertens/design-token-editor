a3d2cc8fb2edbeb5e111fe1afe6fca7cd5960724
Sergei Maertens <sergei@maykinmedia.nl>
2023-01-19 09:08:55 +0100

    :sparkles: Implement goto links for references

--- src/TokenRow.tsx
+++ src/TokenRow.tsx
@@ -12,7 +12,24 @@ export type DesignToken = {
   }
 }
 
-export type TokenRowProps =  {
+interface GoToProps {
+  token: string;
+}
+
+const getTokenHtmlID = (token: string) => `dte-token-${token}`;
+
+
+const GoTo = ({ token }: GoToProps): JSX.Element | null => {
+  const isReference = token.endsWith('}') && token.startsWith('{');
+  if (!isReference) return null;
+  const referencedToken = token.substring(1, token.length - 1);
+  return (
+    <a href={`#${getTokenHtmlID(referencedToken)}`}>goto</a>
+  );
+}
+
+
+export interface TokenRowProps  {
   designToken: DesignToken;
 }
 
@@ -21,12 +38,15 @@ const TokenRow = ({designToken}: TokenRowProps): JSX.Element => {
   const tokenPath = path.join('.');
   return (
     <tr>
-      <td><code>{tokenPath}</code></td>
+      <td id={getTokenHtmlID(tokenPath)}>
+        <code>{tokenPath}</code>
+      </td>
       <td>
         <input name={tokenPath} type="text" placeholder={value} defaultValue="" />
       </td>
       <td>
         <code>{original.value}</code>
+        <GoTo token={original.value} />
       </td>
     </tr>
   );
