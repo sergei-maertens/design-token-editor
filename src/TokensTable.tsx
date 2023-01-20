a5e130735d10fc970c3f0b77beb548136d44a3f8
Sergei Maertens <sergei@maykinmedia.nl>
2023-01-20 10:15:23 +0100

    :sparkles: Implement hiding/collapsing (nested) scopes

--- src/TokensTable.tsx
+++ src/TokensTable.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import isEqual from 'lodash.isequal';
 
 import TokenRow from "./TokenRow";
 import { DesignToken } from "./TokenRow";
@@ -15,14 +16,18 @@ export type DesignTokenContainer = {
 
 interface ScopeRowProps {
   scope: string[];
+  onToggle: (scope: string[]) => void;
 }
 
 
-const ScopeRow = ({ scope=[] }: ScopeRowProps): JSX.Element => {
+const ScopeRow = ({ scope=[], onToggle }: ScopeRowProps): JSX.Element => {
   return (
     <tr>
       <td colSpan={3} style={{border: 'solid 1px grey', padding: '.5em'}}>
-        {scope.join(' / ')}
+        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+          <span>{scope.join(' / ')}</span>
+          <button onClick={() => onToggle(scope)}>toggle children</button>
+        </div>
       </td>
     </tr>
   );
@@ -32,22 +37,39 @@ const ScopeRow = ({ scope=[] }: ScopeRowProps): JSX.Element => {
 interface TokensTableProps {
   container: DesignTokenContainer;
   parentScopes?: string[];
+  closedScopes?: string[][];
+  onToggle: (scope: string[]) => void;
 }
 
-const TokensTableRows = ({ container, parentScopes=[] }: TokensTableProps): JSX.Element => {
-  if ('value' in container) {
-    return (
-      <TokenRow designToken={container as DesignToken} />
-    );
+const TokensTableRows = ({
+    container,
+    parentScopes=[],
+    closedScopes=[],
+    onToggle,
+  }: TokensTableProps): JSX.Element | null => {
+  if ('value' in (container as DesignToken)) {
+    const isHidden = closedScopes.some(closedScope => (
+      (container as DesignToken).path.join('.').startsWith(`{closedScope.join('.')}.`)
+    ));
+    return isHidden ? null : <TokenRow designToken={container as DesignToken} />;
   }
 
   const nested = Object.entries(container).map(
     ([key, child]) => {
       const allScopes = parentScopes.concat(key);
+      const isHidden = allScopes.length >= 1 && closedScopes.some(closedScope => (
+        allScopes.join('.').startsWith(`${closedScope.join('.')}.`)
+      ));
+      if (isHidden) return null;
       return (
         <React.Fragment key={allScopes.join('-')}>
-          { 'value' in child ? null : <ScopeRow scope={allScopes} /> }
-          <TokensTableRows container={child} parentScopes={allScopes} />
+          { 'value' in child ? null : <ScopeRow scope={allScopes} onToggle={onToggle} /> }
+          <TokensTableRows
+            container={child}
+            parentScopes={allScopes}
+            closedScopes={closedScopes}
+            onToggle={onToggle}
+          />
         </React.Fragment>
       );
     }
@@ -57,15 +79,42 @@ const TokensTableRows = ({ container, parentScopes=[] }: TokensTableProps): JSX.
 
 
 const TokensTable = ({ container }: TokensTableProps): JSX.Element => {
+  const namespaces = Object.keys(container).map(namespace => [namespace]);
+  const [closedScopes, setClosedScopes] = useState(namespaces);
+
+  const onToggle = (scope: string[]) => {
+    const isClosed = closedScopes.some(_scope => isEqual(_scope, scope));
+    if (!isClosed) {
+      setClosedScopes([...closedScopes, scope]);
+      return;
+    }
+
+    // remove the scope from the list, but include all the child scopes
+    let nestedContainer = container;
+    for (const bit of scope) {
+      nestedContainer = nestedContainer[bit];
+    }
+    const nestedScopes = Object.keys(nestedContainer).map(key => [...scope, key]);
+    const newClosedScopes = [
+      ...closedScopes.filter(_scope => !isEqual(_scope, scope)),
+      ...nestedScopes,
+    ];
+    setClosedScopes(newClosedScopes);
+    return;
+
+  };
+
+  console.log(closedScopes);
+
   return (
-    <table style={{borderCollapse: 'collapse'}}>
+    <table style={{borderCollapse: 'collapse', width: '100%'}}>
       <tbody>
         <tr>
           <th>Token</th>
           <th>(Default) value</th>
           <th>Value source</th>
         </tr>
-        <TokensTableRows container={container} />
+        <TokensTableRows container={container} closedScopes={closedScopes} onToggle={onToggle} />
       </tbody>
     </table>
   );
