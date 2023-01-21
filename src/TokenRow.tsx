9f8d4822010b92ef97f10b8842098e03e874166a
Sergei Maertens <sergei@maykinmedia.nl>
2023-01-21 21:30:24 +0100

    :fire: Remove goto links

--- src/TokenRow.tsx
+++ src/TokenRow.tsx
@@ -12,22 +12,7 @@ export type DesignToken = {
   }
 }
 
-interface GoToProps {
-  token: string;
-}
-
 const getTokenHtmlID = (token: string) => `dte-token-${token}`;
-
-
-const GoTo = ({ token }: GoToProps): JSX.Element | null => {
-  const isReference = token.endsWith('}') && token.startsWith('{');
-  if (!isReference) return null;
-  const referencedToken = token.substring(1, token.length - 1);
-  return (
-    <a href={`#${getTokenHtmlID(referencedToken)}`}>goto</a>
-  );
-}
-
 
 export interface TokenRowProps  {
   designToken: DesignToken;
@@ -46,7 +31,6 @@ const TokenRow = ({designToken}: TokenRowProps): JSX.Element => {
       </td>
       <td style={{padding: '.5em'}}>
         <code>{original.value}</code>
-        <GoTo token={original.value} />
       </td>
     </tr>
   );
