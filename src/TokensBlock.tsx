28a92da4759eb854416b9364d3751e53e784481b
Sergei Maertens <sergei@maykinmedia.nl>
2023-01-28 20:06:49 +0100

    :sparkles: Display header row in token block

--- src/TokensBlock.tsx
+++ src/TokensBlock.tsx
@@ -11,7 +11,14 @@ const TokensBlock = ({path, tokens}: TokensBlockProps): JSX.Element => {
   return (
     <section className="dte-tokens-block">
       <header className="dte-tokens-block__context-row">{path.join(' ➡️ ')}</header>
+
       <div className="dte-tokens-block__tokens">
+        <div className="dte-token-row dte-token-row--header">
+          <div className="dte-token-row__token-name">Token</div>
+          <div className="dte-token-row__token-value">Value</div>
+          <div className="dte-token-row__token-source">Source / default</div>
+        </div>
+
         {tokens.map(token => (
           <TokenRow key={token.path.join('.')} designToken={token} noWrap />
         ))}
