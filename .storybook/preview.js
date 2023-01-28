7e125d26d8ea9e6dc5636af43b4fbaa09243a879
Sergei Maertens <sergei@maykinmedia.nl>
2023-01-28 16:40:00 +0100

    :hammer: Hook up styles into storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,16 @@
+/**
+ * Ensure you are running `npm start` or have built the CSS using `npm run build:sass`
+ * and `npm run build:design-tokens`.
+ */
+import '../lib/css/dte.css';
+import '../lib/css/index.css';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: {argTypesRegex: '^on[A-Z].*'},
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
