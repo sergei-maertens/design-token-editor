32673a7cb832ae6cd69458a749e567da16c9422e
Sergei Maertens <sergei@maykinmedia.nl>
2023-04-05 12:52:39 +0200

    :label: Add extension information to type definitions

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,28 @@
-export type JSONType =
-  | string
-  | boolean
-  | number
-  | null
-  | JSONType[]
-  | {[key: string]: JSONType};
+export type JSONType = string | boolean | number | null | JSONType[] | JSONObject;
+
+interface JSONObject {
+  [key: string]: JSONType;
+}
+
+interface DesignTokenExtensions {
+  'dte.metadata'?: {
+    isCurated?: boolean;
+    category?: 'color' | 'spacing' | 'component';
+    stateType?: 'userAction' | 'component' | 'appearance';
+  };
+}
+
+interface GroupExtensions {
+  'dte.metadata'?: {
+    groupDescription?: string;
+  };
+}
 
 export interface DesignToken {
   name: string;
   value: string;
+  comment?: string;
+  $extensions?: DesignTokenExtensions;
   original: {
     value: string;
   };
@@ -16,14 +30,16 @@ export interface DesignToken {
   attributes: {
     [key: string]: string;
   };
-  [key: string]: JSONType;
+  [key: string]: any;
 }
 
 /**
  * Key-value mapping, where the value may be a design token (leaf node) or another
  * container.
  */
-export type DesignTokenContainer = DesignToken | {[key: string]: DesignTokenContainer};
+export type DesignTokenContainer =
+  | DesignToken
+  | ({[key: string]: DesignTokenContainer} & {$extensions?: GroupExtensions});
 
 export type TopLevelContainer = {
   [key: string]: DesignTokenContainer;
