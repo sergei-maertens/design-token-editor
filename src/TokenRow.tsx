9da7e249722d0dcca5eaf41e710450f9d1ec82aa
Sergei Maertens <sergei@maykinmedia.nl>
2023-04-05 13:10:57 +0200

    :sparkles: Display comments compiled into tokens file

--- src/TokenRow.tsx
+++ src/TokenRow.tsx
@@ -25,7 +25,7 @@ const TokenRow = ({designToken}: TokenRowProps): JSX.Element => {
   const context = useContext<TokenEditorContextType | null>(TokenEditorContext);
   const [editorMode, setEditorMode] = useState<EditorMode>('documentation');
 
-  const {value, original, path} = designToken;
+  const {value, original, path, comment} = designToken;
   const tokenPath = path.join('.');
 
   const currentValue = context?.tokenValues?.[tokenPath] || value;
@@ -56,59 +56,71 @@ const TokenRow = ({designToken}: TokenRowProps): JSX.Element => {
   }
 
   return (
-    <div className="dte-token-row" id={getTokenHtmlID(tokenPath)}>
-      <div className="dte-token-row__token-name">
-        <span className="dte-code dte-code--inline">{tokenPath}</span>
+    <>
+      <div className="dte-token-row" id={getTokenHtmlID(tokenPath)}>
+        <div className="dte-token-row__token-name">
+          <span className="dte-code dte-code--inline">{tokenPath}</span>
+        </div>
+
+        {comment ? (
+          <p className="dte-token-docs dte-token-docs--inline">{comment}</p>
+        ) : null}
+
+        <div className="dte-kv dte-token-row__token-value-container">
+          <div className="dte-kv__key">Value:</div>
+          <div className="dte-kv__value dte-token-row__token-value">
+            {context ? (
+              <button
+                className="dte-token-row__edit-icon"
+                type="button"
+                onClick={() =>
+                  setEditorMode(
+                    editorMode === 'documentation' ? 'edit' : 'documentation'
+                  )
+                }
+                title="Edit"
+                aria-label="Edit"
+              >
+                {' '}
+                ✏️{' '}
+              </button>
+            ) : null}
+            {editorMode === 'documentation' ? (
+              <span className="dte-code dte-code--inline">{currentValue}</span>
+            ) : (
+              <TokenValueInput
+                name={tokenPath}
+                type={currentValueIsColor ? 'color' : 'text'}
+                defaultTokenValue={value}
+                {...inputProps}
+              />
+            )}
+            {currentValueIsColor && <ColorPreview color={currentValue} />}
+          </div>
+        </div>
+
+        <div className="dte-kv dte-token-row__token-source-container">
+          <div className="dte-kv__key" title="Source of (default) value">
+            Source:
+          </div>
+          <div
+            className={clsx('dte-kv__value', 'dte-token-row__token-source', {
+              'dte-token-row__token-source--color': originalValueIsColor,
+            })}
+          >
+            {originalValueIsColor ? (
+              <ColorPreview color={original.value} />
+            ) : (
+              <span className="dte-code dte-code--inline">{original.value}</span>
+            )}
+          </div>
+        </div>
       </div>
 
-      <div className="dte-kv dte-token-row__token-value-container">
-        <div className="dte-kv__key">Value:</div>
-        <div className="dte-kv__value dte-token-row__token-value">
-          {context ? (
-            <button
-              className="dte-token-row__edit-icon"
-              type="button"
-              onClick={() =>
-                setEditorMode(editorMode === 'documentation' ? 'edit' : 'documentation')
-              }
-              title="Edit"
-              aria-label="Edit"
-            >
-              {' '}
-              ✏️{' '}
-            </button>
-          ) : null}
-          {editorMode === 'documentation' ? (
-            <span className="dte-code dte-code--inline">{currentValue}</span>
-          ) : (
-            <TokenValueInput
-              name={tokenPath}
-              type={currentValueIsColor ? 'color' : 'text'}
-              defaultTokenValue={value}
-              {...inputProps}
-            />
-          )}
-          {currentValueIsColor && <ColorPreview color={currentValue} />}
-        </div>
-      </div>
-
-      <div className="dte-kv dte-token-row__token-source-container">
-        <div className="dte-kv__key" title="Source of (default) value">
-          Source:
-        </div>
-        <div
-          className={clsx('dte-kv__value', 'dte-token-row__token-source', {
-            'dte-token-row__token-source--color': originalValueIsColor,
-          })}
-        >
-          {originalValueIsColor ? (
-            <ColorPreview color={original.value} />
-          ) : (
-            <span className="dte-code dte-code--inline">{original.value}</span>
-          )}
-        </div>
-      </div>
-    </div>
+      {comment ? (
+        <p className="dte-token-docs dte-token-docs--bottom">{comment}</p>
+      ) : null}
+    </>
   );
 };
 
