a219c8ad0c4bb75de9f1faf9358e5e91da9679de
Sergei Maertens <sergei@maykinmedia.nl>
2023-11-29 12:06:14 +0100

    :bug: Ignore unparsable CSS color values
    Otherwise the token row crashes on these.

--- src/ColorPreview.tsx
+++ src/ColorPreview.tsx
@@ -4,12 +4,14 @@ interface ColorPreviewProps {
   color: string;
 }
 
+const UNPARSABLE_COLOR_VALUES = ['inherit', 'initial', 'unset'];
+
 // heuristic based on token name/scope and maybe even the format of the value
 export const isColor = (value: string): boolean => {
   // taken from https://stackoverflow.com/a/56266358/973537
   const s = new Option().style;
   s.color = value;
-  return s.color !== '';
+  return s.color !== '' && !UNPARSABLE_COLOR_VALUES.includes(s.color);
 };
 
 const ColorPreview = ({color}: ColorPreviewProps): JSX.Element | null => {
